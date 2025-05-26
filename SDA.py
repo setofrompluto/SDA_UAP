7110c78a59d5b0422d2acbc8d72d542ce918dd9c
Yoesefall <84967496+YusufAlif@users.noreply.github.com>
2025-05-27 02:27:06 +0700

    Update SDA.py

--- SDA.py
+++ SDA.py
@@ -1,1 +1,13 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+from PIL import Image, ImageTk
 
+class AplikasiTkinter:
+  def __init__ (self, root):
+    self.root = root
+
+
+if __name__ == "__main__":
+  root = tk.Tk()
+  app = AplikasiTkinter(root)
+  root.mainloop()
