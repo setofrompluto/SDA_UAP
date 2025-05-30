f1d487b048a0fe2b8ad0db7142b2ff4dd065af29
chayyarra <chayya.igda.897@gmail.com>
2025-05-30 09:18:07 -0700

    Update SDA.py

--- SDA.py
+++ SDA.py
@@ -6,22 +6,23 @@ class AplikasiTkinter:
     def __init__(self, root):
         self.root = root
         self.root.title("Aplikasi Tkinter Sederhana")
-        self.root.geometry("700x400")
+        self.root.geometry("400x400")
         self.root.resizable(False, False)
         
         self.frame_awal = tk.Frame(root, bg="white")
         self.frame_awal.pack(fill="both", expand=True)
         
-        self.label_nama = tk.Label(self.frame_awal, text="Welcome To Our Projeck!",font=("Helvetica", 18), bg="white")
-        self.label_nama.pack(pady=50)
+        self.label_nama = tk.Label(self.frame_awal, text="Welcome To Our Projeck!",font=("Courier New", 20, "bold"), bg="white")
+        self.label_nama.pack(pady=100)
 
-        self.btn_masuk = tk.Button(self.frame_awal, text="Click To start", font=("Helvetica", 14), command=self.buka_halaman_utama)
+        self.btn_masuk = tk.Button(self.frame_awal, text="Click To start", font=("Courier New", 14,"italic"), command=self.buka_halaman_utama)
         self.btn_masuk.pack()
 
         self.frame_utama = tk.Frame(root)
     
     def buka_halaman_utama(self):
         self.frame_awal.pack_forget()
+        self.root.state("zoomed")
         self.tampilkan_halaman_utama()
 
     def tampilkan_halaman_utama(self):
@@ -33,6 +34,8 @@ class AplikasiTkinter:
         
         self.label_judul = tk.Label(root, text="Testing How This Work", font=("Helvetica", 16))
         self.label_judul.pack(pady=10)
+        
+        self.tampilkan_gambar()
 
     def tampilkan_gambar(self):
         img = Image.open("test.png")
